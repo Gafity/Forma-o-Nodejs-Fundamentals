61e6a511cbbd83aa15206473a79ee50c193d05c0
Gabriel Gama <gafityr@gmail.com>
2026-06-20 20:37:53 -0300

    create podcast service

---
+++ arch/src/services/list_episodes-service.ts
@@ -0,0 +1,6 @@
+import { repositoryPodcast } from "../repository/podcasts-repository";
+
+export const serviceListEpisodes = async () => {
+  const data = await repositoryPodcast();
+  return data;
+};
