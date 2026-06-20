5075fa6ac2cd7e080341b0081cf8c6e0387919d9
Gabriel Gama <gafityr@gmail.com>
2026-06-20 20:37:11 -0300

    create podcasts-repository

---
+++ arch/src/repository/podcasts-repository.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+import path from "path";
+
+interface PodcastModel {
+  podcastName: string;
+  episode: string;
+  videoId: string;
+  categories: string[];
+}
+
+const pathData = path.join(__dirname, "../repository/podcasts.json");
+
+export const repositoryPodcast = async (
+  podcastName?: string,
+): Promise<PodcastModel> => {
+  const rawData = fs.readFileSync(pathData, "utf-8");
+  const jsonFile = JSON.parse(rawData);
+
+  if (podcastName) {
+    const podcastNameJson = jsonFile.filter(
+      (podcast: PodcastModel) => podcast.podcastName === podcastName,
+    );
+    return podcastNameJson;
+  }
+
+  return jsonFile;
+};
