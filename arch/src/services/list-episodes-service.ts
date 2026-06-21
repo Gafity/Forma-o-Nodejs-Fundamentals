618c34546df52b05b9ab6c369a5ac5f56d96592f
Gabriel Gama <gafityr@gmail.com>
2026-06-21 11:26:05 -0300

    refactor list episodes service

--- arch/src/services/list-episodes-service.ts
+++ arch/src/services/list-episodes-service.ts
@@ -1,9 +1,23 @@
-import type { IncomingMessage } from "http";
 import { repositoryPodcast } from "../repository/podcasts-repository";
+import type { FilterPodCastModel } from "../models/filter-podcast-model";
+import { StatusCode } from "../utils/status-codes";
 
 export const serviceFilterEpisodes = async (
   podcastName: string | undefined,
-) => {
+): Promise<FilterPodCastModel> => {
+  let responseFormat: FilterPodCastModel = {
+    statusCode: 0,
+    body: [],
+  };
+
   const queryString = podcastName?.split("?p=")[1] ?? "";
   const data = await repositoryPodcast(podcastName);
+
+  if (data) {
+    responseFormat.statusCode = StatusCode.OK;
+  } else {
+    responseFormat.statusCode = StatusCode.NO_CONTENT;
+  }
+  responseFormat.body = [data];
+  return responseFormat;
 };
